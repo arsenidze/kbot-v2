7e47704f30c54a8528bbb56db55412316dd2466b
arsenidze <melihovarsenij@gmail.com>
2024-08-05 12:08:00 +0300

    try test step

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,17 +1,13 @@
 pipeline {
     agent any
     parameters {
-
         choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
-
+        choice(name: 'ARCH', choices: ['amd64', 'arm64'], description: 'Pick ARCH')
     }
     stages {
-        stage('Example') {
+        stage('Test') {
             steps {
-                echo "Build for platform ${params.OS}"
-
-                echo "Build for arch: ${params.ARCH}"
-
+                make test
             }
         }
     }
